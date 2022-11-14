8f463b4745f76d4b131522efe407b5b74913dfab
Kyle <kyle.vavasour@gmail.com>
2022-11-14 17:58:39 -0500

    Added bidding interactor and methods to buyable and rentable interfaces.

--- src/main/java/Properties/Interfaces/Buyable.java
+++ src/main/java/Properties/Interfaces/Buyable.java
@@ -1,4 +1,9 @@
 package Properties.Interfaces;
 
 public interface Buyable {
+    float estimateMortgage();
+    float getAskingPrice();
+    float getCurrentBuyBid();
+    float getMinimumBuyBid();
+    void setBuyBid();
 }
--- src/main/java/Properties/Interfaces/Rentable.java
+++ src/main/java/Properties/Interfaces/Rentable.java
@@ -1,4 +1,8 @@
 package Properties.Interfaces;
 
 public interface Rentable {
+    float getRentalPrice();
+    float getCurrentRentBid();
+    float getMinimumRentBid();
+    void setRentBid();
 }
