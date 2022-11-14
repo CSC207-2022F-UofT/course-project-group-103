0b054bf8c009b3afd486db63b2bfed3c0345ab66
jliqian <juliet.li@mail.utoronto.ca>
2022-11-14 17:04:26 -0500

    add branch interactors and preliminary PropertyManager and Owner class

--- src/main/java/Managers/PropertyManager.java
+++ src/main/java/Managers/PropertyManager.java
@@ -1,4 +1,26 @@
 package Managers;
 
+import Properties.Property;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PropertyManager {
+
+    List<Property> properties = new ArrayList<Property>();
+    public boolean addProperty(Property p) {
+
+    }
+
+    public boolean removeProperty (String id) {
+
+    }
+
+    public boolean editProperty (String id, Property p) {
+
+    }
+
+    public Property getProperty (String id) {
+
+    }
 }
--- src/main/java/Users/Owner.java
+++ src/main/java/Users/Owner.java
@@ -1,4 +1,21 @@
 package Users;
 
-public class Owner {
+import Managers.PropertyManager;
+
+import java.util.List;
+
+public class Owner extends User {
+    PropertyManager p;
+    String contact_info;
+
+    public Owner() {
+        super();
+    }
+    public List[] getReviews() {
+
+    }
+
+    public void addContact (String contact_info) {
+        this.contact_info = contact_info;
+    }
 }
