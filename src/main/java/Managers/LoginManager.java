910513eed836c40dd9634f87bd9e8b4d6de92673
eric897 <ericxyz.shi@mail.utoronto.ca>
2022-11-21 05:00:20 -0500

    Add saveToRealtorListing method in LoginManager

--- src/main/java/Managers/LoginManager.java
+++ src/main/java/Managers/LoginManager.java
@@ -1,4 +1,18 @@
 package Managers;
 
+import org.json.JSONObject;
+import java.io.FileWriter;
+import Users.*;
+
 public class LoginManager {
+    public void saveToRealtorListing(Realtor realtor) {
+        JSONObject realtorObject = new JSONObject();
+        realtorObject.put(String.valueOf(realtor.getId()), realtor.getName());
+
+        try (FileWriter file = new FileWriter("src\\main\\Databases\\RealtorListing.json")) {
+            file.write(realtorObject.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
