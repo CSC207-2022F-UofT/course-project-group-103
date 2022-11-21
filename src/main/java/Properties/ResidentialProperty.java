93781c8afc5071a07ba3f5c9f7a589a078d2b0ac
jliqian <juliet.li@mail.utoronto.ca>
2022-11-20 22:36:48 -0500

    basic get methods and setup abstract class ResidentialProperty

--- src/main/java/Properties/ResidentialProperty.java
+++ src/main/java/Properties/ResidentialProperty.java
@@ -1,4 +1,33 @@
 package Properties;
 
-public class ResidentialProperty extends Property{
+public abstract class ResidentialProperty extends Property {
+    private int kitchens;
+    private int bedrooms;
+    private int bathrooms;
+    private int laundry;
+
+    public ResidentialProperty(String name, String address, String ID, String owner, int sqFt, int price,
+                               int kitchens, int bedrooms, int bathrooms, int laundry) {
+        super(name, address, ID, owner, sqFt, price);
+        this.kitchens = kitchens;
+        this.bedrooms = bedrooms;
+        this.bathrooms = bathrooms;
+        this.laundry = laundry;
+    }
+
+    public int getKitchens() {
+        return kitchens;
+    }
+
+    public int getBedrooms() {
+        return bedrooms;
+    }
+
+    public int getBathrooms() {
+        return bathrooms;
+    }
+
+    public int getLaundry() {
+        return laundry;
+    }
 }
