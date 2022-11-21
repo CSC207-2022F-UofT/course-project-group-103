16ae9584b2c895c5f95925ce23dd6d1d220752e0
jliqian <juliet.li@mail.utoronto.ca>
2022-11-20 22:16:06 -0500

    basic get and set methods for PropertyManager

--- src/main/java/Managers/PropertyManager.java
+++ src/main/java/Managers/PropertyManager.java
@@ -6,20 +6,73 @@ import java.util.ArrayList;
 
 public class PropertyManager {
 
-    ArrayList<Property> properties = new ArrayList<Property>();
-    public boolean addProperty(Property p) {
+    private ArrayList<Property> properties;
 
+    public PropertyManager() {
+        properties = new ArrayList<Property>();
     }
 
+    /**
+     * Adds new property to the Arraylist
+     * @param p - Given property to add
+     * @return boolean if the addition was successful, or false if a Property with the identical ID
+     *          already exists
+     */
+    public boolean addProperty(Property p) {
+        if (properties.size() == 0) {
+            properties.add(p);
+        } else {
+            for (int i = 0; i < properties.size(); i++)
+                if (properties.get(i).getID() == p.getID()) {
+                    return false;
+                }
+            properties.add(p);
+        }
+        return true;
+    }
+
+    /**
+     * Removes given property based on ID
+     * @param id - String identifier for each property
+     * @return boolean indicating if the property has been removed
+     */
     public boolean removeProperty (String id) {
 
+        for (int i = 0; i < properties.size(); i++)
+            if (properties.get(i).getID() == id) {
+                properties.remove(i);
+                return true;
+            }
+        return false;
     }
 
+    /**
+     * Edits given property based on id, replaces with a new Property object
+     * @param id - String identifier for each property
+     * @param p - Property replacing the outdated property
+     * @return
+     */
     public boolean editProperty (String id, Property p) {
-
+        for (int i = 0; i < properties.size(); i++)
+            if (properties.get(i).getID() == id) {
+                properties.set(i, p);
+                return true;
+            }
+        return false;
     }
 
+    /**
+     * Gets a given property based on the Property ID
+     * @param id - String identifier for each property
+     * @return Property located by the ID or null if the property was not inside the list
+     */
     public Property getProperty (String id) {
-
+        for (int i = 0; i < properties.size(); i++)
+            if (properties.get(i).getID() == id) {
+                return properties.get(i);
+            }
+        return null;
     }
+
+
 }
