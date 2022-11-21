992ff66fdc011048e969aedd870bfe7eeb9b2de2
jliqian <juliet.li@mail.utoronto.ca>
2022-11-21 00:22:11 -0500

    fix Restaurant

--- src/main/java/Properties/Restaraunt.java
+++ src/main/java/Properties/Restaraunt.java
@@ -4,7 +4,8 @@ public class Restaurant extends CommercialProperty {
     private String specifications;
     public Restaurant (String name, String address, String ID, String owner, int sqFt, int price,
                    int land_size,
-                   int office_rooms, int reception) {
+                   int office_rooms, int reception,
+                       String specifications) {
         super(name, address, ID, owner, sqFt, price, land_size);
         this.specifications = specifications;
     }
