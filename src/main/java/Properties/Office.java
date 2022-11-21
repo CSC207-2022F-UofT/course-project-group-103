2d8d405b16e72ef7b4a4f5ce12abfcdd48ce4885
jliqian <juliet.li@mail.utoronto.ca>
2022-11-21 00:01:51 -0500

    basic get methods and setup CommercialProperty and child classes.

--- src/main/java/Properties/Office.java
+++ src/main/java/Properties/Office.java
@@ -1,4 +1,22 @@
 package Properties;
 
 public class Office extends CommercialProperty {
+
+    private int office_rooms;
+    private int reception;
+    public Office (String name, String address, String ID, String owner, int sqFt, int price,
+                   int land_size,
+                   int office_rooms, int reception) {
+        super(name, address, ID, owner, sqFt, price, land_size);
+        this.office_rooms = office_rooms;
+        this.reception = reception;
+    }
+
+    public int getOffice_rooms() {
+        return office_rooms;
+    }
+
+    public int getReception() {
+        return reception;
+    }
 }
