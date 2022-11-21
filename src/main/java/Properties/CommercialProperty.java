3ae1c249371bc486c0d51ca30ab2e2ff85f4705d
jliqian <juliet.li@mail.utoronto.ca>
2022-11-21 00:01:38 -0500

    basic get methods and setup CommercialProperty and child classes.

--- src/main/java/Properties/CommercialProperty.java
+++ src/main/java/Properties/CommercialProperty.java
@@ -1,4 +1,14 @@
 package Properties;
 
 public class CommercialProperty extends Property{
+    private int land_size;
+    public CommercialProperty(String name, String address, String ID, String owner, int sqFt, int price,
+                              int land_size) {
+        super(name, address, ID, owner, sqFt, price);
+        this.land_size = land_size;
+    }
+
+    public int getLand_size() {
+        return land_size;
+    }
 }
--- src/main/java/Properties/Restaraunt.java
+++ src/main/java/Properties/Restaraunt.java
@@ -1,4 +1,16 @@
 package Properties;
 
 public class Restaurant extends CommercialProperty {
+    private String specifications;
+    public Restaurant (String name, String address, String ID, String owner, int sqFt, int price,
+                   int land_size,
+                   int office_rooms, int reception) {
+        super(name, address, ID, owner, sqFt, price, land_size);
+        this.specifications = specifications;
+    }
+
+    public String getSpecifications() {
+        return specifications;
+    }
 }
+
