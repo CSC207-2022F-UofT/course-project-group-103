3498f0fd49d255a000c5830e2ed8df15082abf96
eric897 <ericxyz.shi@mail.utoronto.ca>
2022-11-20 22:23:40 -0500

    Set up ClientManager

--- src/main/java/Managers/ClientManager.java
+++ src/main/java/Managers/ClientManager.java
@@ -1,4 +1,23 @@
 package Managers;
+import java.util.ArrayList;
+import Users.*;
 
 public class ClientManager {
+    private ArrayList<User> clientList = new ArrayList<>();
+
+    public ClientManager(ArrayList<User> clientList){
+        this.clientList = clientList;
+    }
+
+    public ArrayList<User> getClients() {
+        return clientList;
+    }
+
+    public void addClient(User client) {
+        clientList.add(client);
+    }
+
+    public void removeClient(User client) {
+        clientList.remove(client);
+    }
 }
