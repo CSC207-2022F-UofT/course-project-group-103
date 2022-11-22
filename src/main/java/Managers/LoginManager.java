2eae312ffc146c36ea7bb501a2e22d9a6ee38170
eric897 <ericxyz.shi@mail.utoronto.ca>
2022-11-21 23:27:35 -0500

    Update LoginManager

--- src/main/java/Managers/LoginManager.java
+++ src/main/java/Managers/LoginManager.java
@@ -1,5 +1,6 @@
 package Managers;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import java.io.FileWriter;
 import Users.*;
@@ -7,7 +8,11 @@ import Users.*;
 public class LoginManager {
     public void saveToRealtorListing(Realtor realtor) {
         JSONObject realtorObject = new JSONObject();
-        realtorObject.put(String.valueOf(realtor.getID()), realtor.getName());
+        JSONObject realtorObject2 = new JSONObject();
+        realtorObject2.put(String.valueOf("name"), realtor.getName());
+        realtorObject2.put(String.valueOf("contact"), realtor.getContact());
+
+        realtorObject.put(realtor.getID(), realtorObject2);
 
         try (FileWriter file = new FileWriter("src\\main\\Databases\\RealtorListing.json")) {
             file.write(realtorObject.toString());
