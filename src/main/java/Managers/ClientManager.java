814097390b947039dc4cfb0552dc42da829c68e1
eric897 <ericxyz.shi@mail.utoronto.ca>
2022-11-21 23:30:53 -0500

    Remove ClientManager methods

--- src/main/java/Managers/ClientManager.java
+++ src/main/java/Managers/ClientManager.java
@@ -1,23 +1,5 @@
 package Managers;
-import java.util.ArrayList;
-import Users.*;
 
 public class ClientManager {
-    private ArrayList<User> clientList = new ArrayList<>();
 
-    public ClientManager(ArrayList<User> clientList){
-        this.clientList = clientList;
-    }
-
-    public ArrayList<User> getClients() {
-        return clientList;
-    }
-
-    public void addClient(User client) {
-        clientList.add(client);
-    }
-
-    public void removeClient(User client) {
-        clientList.remove(client);
-    }
 }
