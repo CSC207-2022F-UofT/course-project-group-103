c988d28ce853c8c79775652a8cf299b62e5b2b79
ZeinSulayman <zein.sulayman@mail.utoronto.ca>
2022-11-22 00:24:47 -0500

    feature 6 (AccessSingleProperty) Push 4 from feature-6 local branch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,16 @@
+import Exceptions.UndefinedUserType;
+import Managers.PropertyManager;
+import Users.User;
+
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args) throws UndefinedUserType, IOException {
+        User user = PropertyManager.getUser("4");
+        System.out.println(user.getName());
+        User user1 = PropertyManager.getUser("1");
+        System.out.println(user1.getName());
+        User user2 = PropertyManager.getUser("2");
+        System.out.println(user2.getName());
+    }
 }
