117a1a776670a35de869418c7a7cb4cf015501da
eric897 <ericxyz.shi@mail.utoronto.ca>
2022-12-03 02:05:58 -0500

    reset main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import Interactors.RealtorSearchInteractor;
 import Managers.LoginManager;
 import Users.Owner;
 import Users.Realtor;
@@ -7,14 +8,5 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("niggerrrrrrrrrrrrrrrr");
-        LoginManager lm = new LoginManager();
-//        lm.saveToRealtorListing(new Realtor("123", "lol", "lmao1234",
-//                "lol@lol.com", "a", "z"));
-//        lm.saveToRealtorListing(new Realtor("124", "aasdfsadf", "lmao1235",
-//                "sdfgdfsg@lol.com", "b", "y"));
-        Realtor test = lm.getRealtor(2);
-        System.out.println(test.getName());
-
     }
 }
