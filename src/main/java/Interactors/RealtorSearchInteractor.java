e0820bb8c0a8e4dfc91d166fa5cb84b769c2d405
eric897 <ericxyz.shi@mail.utoronto.ca>
2022-12-03 02:02:00 -0500

    Create UserGateway and RealtorSearchInteractor

--- src/main/java/Interactors/RealtorSearchInteractor.java
+++ src/main/java/Interactors/RealtorSearchInteractor.java
@@ -1,30 +1,12 @@
 package Interactors;
 
 import Users.Realtor;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import Managers.LoginManager;
 
 public class RealtorSearchInteractor {
     public ArrayList<Realtor> listRealtors() throws IOException {
-        LoginManager loginManager = new LoginManager();
-        ArrayList<Realtor> realtors = new ArrayList<>();
-
-        String jsonString = Files.readString(Paths.get("src\\main\\Databases\\UserListing.json"));
-        JSONObject users = new JSONObject(jsonString);
-        JSONArray keys = users.names();
-
-        for (int i = 0; i < keys.length(); i++) {
-            String key = keys.getString(i);
-            Realtor realtor = loginManager.getRealtor(key);
-            if (realtor != null) {
-                realtors.add(realtor);
-            }
-        }
-        return realtors;
+        UserGateway userGateway = new UserGateway();
+        return userGateway.getRealtors();
     }
 }
