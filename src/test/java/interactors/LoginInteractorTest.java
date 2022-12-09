bb08bd4c62b74519ca57f8ca529d846a45acc385
SeanMalla <sean.malla@mail.utoronto.ca>
2022-12-08 23:28:41 -0500

    added some test cases. going to finish in a bit

---
+++ src/test/java/interactors/LoginInteractorTest.java
@@ -0,0 +1,17 @@
+package interactors;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class LoginInteractorTest {
+
+    @Test
+    void loginTest() {
+
+
+    }
+
+    @Test
+    void getSecurityQuestionTest() {
+    }
+}
---
+++ src/test/java/interactors/SignUpInteractorTest.java
@@ -0,0 +1,20 @@
+package interactors;
+
+import interactors.exceptions.MessengerNotFound;
+import interactors.exceptions.UndefinedUserType;
+import managers.LoginManager;
+import org.junit.jupiter.api.Test;
+import screens.GUI;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SignUpInteractorTest {
+
+    @Test
+    void signUpTest() throws MessengerNotFound, UndefinedUserType, IOException {
+
+
+    }
+}
